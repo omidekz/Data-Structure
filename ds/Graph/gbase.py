9cc0fc8973c1cf1e22bbd06440c5f04bdd92b450
Omid Kz <omidekz@gmail.com>
2019-08-23 19:16:26 +0430

    create graph base class

--- ds/Graph/gbase.py
+++ ds/Graph/gbase.py
@@ -1,7 +1,4 @@
 class GBase:
-    def __init__(self):
-        self._list = list()
-
     def add(self, obj): pass
 
     def search(self, key): pass
@@ -9,4 +6,3 @@ class GBase:
     def remove(self, key): pass
 
     def __iter__(self): pass
-
